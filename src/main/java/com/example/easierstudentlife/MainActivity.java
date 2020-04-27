023b28ba123ce1a22709a7f8053333a9d65997f7
kevin33579 <64402706+kevin33579@users.noreply.github.com>
2020-04-27 16:28:47 +0700

    Add files via upload

---
+++ src/main/java/com/example/easierstudentlife/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.easierstudentlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.co.jeteas.easierstudentlife.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
