c93e478167b727ec94cd0520a5a992d8e90ce566
rioglorious <rioglorious@gmail.com>
2020-05-09 16:40:35 +0700

    Profile fragment UI

--- app/src/main/java/com/example/progtech/easierstudentlife/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/progtech/easierstudentlife/fragment/ProfileFragment.java
@@ -2,26 +2,40 @@ package com.example.progtech.easierstudentlife.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.progtech.easierstudentlife.R;
 import com.example.progtech.easierstudentlife.model.UserData;
 import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment<Updated> extends Fragment {
+    private TextView usernameView, majorView, univView, taskView, subjectView, activityView, sksView;
+    private FirebaseAuth mAuth;
+    private ImageView userImageView;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
 
-    UserData userData;
-    public ProfileFragment(UserData user) {
-        userData = user;
-    }
-
+//    UserData userData;
+//    public ProfileFragment(UserData user) {
+//        userData = user;
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,6 +45,7 @@ public class ProfileFragment extends Fragment {
 
         MaterialTextView toolbar_title = getActivity().findViewById(R.id.toolBar_title);
         toolbar_title.setText("Profile");
+
 
         return view;
     }
